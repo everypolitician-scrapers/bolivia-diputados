e1265a32871babc9f8a968a3c280a9bc8b5f8626
Tony Bowden <tony@tmtm.com>
2015-09-10 16:30:25 +0100

    Better area handling

--- scraper.rb
+++ scraper.rb
@@ -23,12 +23,14 @@ def scrape_list(url)
   noko = noko_for(url)
   deps = noko.css('.box-dip').map do |box|
     name = box.css('p#nombre').text
+
     data = { 
       name: name,
       image: box.css('img/@src').text,
+      dpto:  box.xpath('.//p[contains(.,"Dpto:")]').text.to_s.sub('Dpto: ',''),
+      dip:   box.xpath('.//p[contains(.,"Dip. ")]').text.to_s.sub('Dip. ',''),
       party: box.xpath('.//p[contains(.,"Bancada:")]').text.to_s.sub('Bancada: ',''),
-      area: box.xpath('.//p[contains(.,"nominal")]').text.to_s.sub('Dip. ','').sub('Uninominal ',''),
-      type: box.xpath('.//p//strong').text.to_s,
+      type:  box.xpath('.//p//strong').text.to_s,
     }.reject { |_, v| v.nil? || v.to_s.empty? }
     data[:image] = URI.join(url, URI.encode(data[:image])).to_s unless data[:image].to_s.empty?
     data[:id] = File.basename(data[:image], '.*')
@@ -37,6 +39,14 @@ def scrape_list(url)
 
   deps.group_by { |d| d[:id] }.each do |d, ds| 
     data = ds.reduce(&:merge).merge({ term: 2015, source: url })
+    if data[:dip].include? ('Uninominal')
+      data[:area_id] = data[:dip].sub('Uninominal ','')
+      data[:area] = data[:dpto]
+    elsif %w(Plurinominal Especial).include? data[:dip] 
+      data[:area] = data[:dpto]
+    else
+      raise "Unknown area"
+    end
     ScraperWiki.save_sqlite([:id, :term], data)
   end
  
